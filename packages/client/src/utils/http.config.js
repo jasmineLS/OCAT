2cf628635c92bc3c9285dbdf008fcf699b61c200
JasmineLS <jasminelimsmith30@gmail.com>
2025-05-13 13:36:08 -0400

    fixed table to display all information

--- packages/client/src/utils/http.config.js
+++ packages/client/src/utils/http.config.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: `http://localhost:3000/api`, // Ensure this matches your backend base URL
-  timeout: 5000, // Optional: Set a timeout for requests
+  baseURL: `http://localhost:4000/api`, // Ensure this matches your backend server's URL and port
+  headers: {
+    'Content-Type': `application/json`,
+  },
+  timeout: 10000,
 });
 
 export default Axios;
